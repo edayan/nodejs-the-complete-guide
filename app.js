6119b946069ef9e06b82b15b5272a64bb7fce7a6
Saju Paul <saju.christel@blu-pa.com>
2019-10-10 21:24:48 +0530

    aading two routing

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
-const http = require('http');
 const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('in the middleware');
-  next();
+app.use('/add-product', (req, res, next) => {
+  res.send('<h1>hello add product</h1>');
 });
 
-app.use((req, res, next) => {
-  console.log('in another middleware');
-  res.send('<h1>hello from express</h1>');
+app.use('/', (req, res, next) => {
+  res.send('<h1>hello edayan from express</h1>');
 });
 
 app.listen(3000);
