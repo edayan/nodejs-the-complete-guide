38ad7234ab8ede17aa6d3204ee78c151f2c62e13
Saju Paul <saju.christel@blu-pa.com>
2019-10-11 10:09:42 +0530

    adding express Router

--- app.js
+++ app.js
@@ -3,21 +3,12 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/add-product', (req, res, next) => {
-  res.send(
-    '<form action="/product" method="POST"><input type="text" name="title"/><button type="submit">Add product</button></form>'
-  );
-});
-
-app.post('/product', (req, res) => {
-  console.log(req.body);
-  res.redirect('/');
-});
-
-app.use('/', (req, res, next) => {
-  res.send('<h1>hello edayan from express</h1>');
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.listen(3000);
