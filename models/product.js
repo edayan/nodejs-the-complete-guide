cd17dff1e482dadaf4bd1b6637e08969983baeb0
Saju Paul <saju.christel@blu-pa.com>
2019-10-17 19:10:59 +0530

    read from file and display content

--- models/product.js
+++ models/product.js
@@ -26,6 +26,16 @@ module.exports = class Product {
   }
 
   static fetchAll() {
-    return products;
+    const p = path.join(
+      path.dirname(process.mainModule.filename),
+      'data',
+      'products.json'
+    );
+    fs.readFile(p, (err, fileContent) => {
+      if (err) {
+        return [];
+      }
+      return JSON.parse(fileContent);
+    });
   }
 };
