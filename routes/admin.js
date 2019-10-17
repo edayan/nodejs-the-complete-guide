ace45ee3be5a1b5de8b4e47fb1bf61a491572c2b
Saju Paul <saju.christel@blu-pa.com>
2019-10-17 17:20:29 +0530

    complete views with ejs

--- routes/admin.js
+++ routes/admin.js
@@ -8,12 +8,20 @@ const router = express.Router();
 
 const products = [];
 
+// /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+  res.render('add-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true
+  });
 });
 
-router.post('/product', (req, res) => {
-  products.push({title: req.body.title});
+// /admin/add-product => POST
+router.post('/add-product', (req, res, next) => {
+  products.push({ title: req.body.title });
   res.redirect('/');
 });
 
