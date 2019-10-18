1796fe477a18d9e12d7a6113d3aa95bf6a264a6c
Saju Paul <saju.christel@blu-pa.com>
2019-10-18 17:33:34 +0530

    Display specific product

--- controllers/shop.js
+++ controllers/shop.js
@@ -11,11 +11,15 @@ exports.getProducts = (req, res, next) => {
 };
 
 exports.getProduct = (req, res, next) => {
-  const prodId= req.params.productId;
-  Product.findById(prodId, product=> {
-    console.log(product);
+  const prodId = req.params.productId;
+  Product.findById(prodId, product => {
+    res.render('shop/product-detail', {
+      product: product,
+      pageTitle: product.title,
+      path: '/products'
+    });
   });
-}
+};
 
 exports.getIndex = (req, res, next) => {
   Product.fetchAll(products => {
@@ -47,4 +51,3 @@ exports.getCheckout = (req, res, next) => {
     pageTitle: 'Checkout'
   });
 };
-
