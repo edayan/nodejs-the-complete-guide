c09e65f63d7a800305ae3f7d5d716da917c94e7c
Saju Paul <saju.christel@blu-pa.com>
2019-10-18 14:24:28 +0530

    adding new page orders

--- controllers/shop.js
+++ controllers/shop.js
@@ -22,14 +22,20 @@ exports.getIndex = (req, res, next) => {
 
 exports.getCart = (req, res, next) => {
   res.render('shop/cart', {
-    path:'/cart',
+    path: '/cart',
     pageTitle: 'Your Cart'
-  })
-}
+  });
+};
 
+exports.getOrders = (req, res, next) => {
+  res.render('shop/orders', {
+    path: '/orders',
+    pageTitle: 'Your Orders'
+  });
+};
 exports.getCheckout = (req, res, next) => {
   res.rener('/shop/checkout', {
     pth: '/checkout',
     pageTitle: 'Checkout'
-  })
-}
+  });
+};
