355ea20d91d44d3d6d465814a84c752623cdc9f5
Saju Paul <saju.christel@blu-pa.com>
2019-10-18 20:01:47 +0530

    Adding edit-product routing

--- routes/admin.js
+++ routes/admin.js
@@ -12,6 +12,8 @@ router.get('/add-product', adminController.getAddProduct);
 // /admin/add-product => POST
 router.post('/add-product', adminController.postAddProduct);
 
+router.get('/edit-product/:productId', adminController.getEditProduct);
+
 router.get('/products', adminController.getProducts);
 
 module.exports = router;
