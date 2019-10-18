3a4e81ae871c5ce86cfb647034349fadc653bbf9
Saju Paul <saju.christel@blu-pa.com>
2019-10-18 13:07:41 +0530

    adding fields imageUrl, price and description

--- controllers/admin.js
+++ controllers/admin.js
@@ -11,11 +11,15 @@ exports.getAddProduct = (req, res, next) => {
 };
 
 exports.postAddProduct = (req, res, next) => {
-  const product = new Product(req.body.title);
+  const title = req.body.title;
+  const imageUrl = req.body.imageUrl;
+  const price = req.body.price;
+  const description = req.body.description;
+
+  const product = new Product(title, imageUrl, price, description);
   product.save();
   res.redirect('/');
 };
-
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
@@ -25,4 +29,4 @@ exports.getProducts = (req, res, next) => {
       path: '/admin/products'
     });
   });
-}
+};
