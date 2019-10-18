ef6b70fc82cf0fc2203a436280faf95ada3edb26
Saju Paul <saju.christel@blu-pa.com>
2019-10-18 13:23:28 +0530

    listing books in admin page also

--- routes/admin.js
+++ routes/admin.js
@@ -12,6 +12,6 @@ router.get('/add-product', adminController.getAddProduct);
 // /admin/add-product => POST
 router.post('/add-product', adminController.postAddProduct);
 
-router.get('products', adminController.getProducts);
+router.get('/products', adminController.getProducts);
 
 module.exports = router;
