86b75301e523080764d81d455233828a390ce2d7
Saju Paul <saju.christel@blu-pa.com>
2019-10-25 22:16:44 +0530

    clear the cart after ordering

--- controllers/shop.js
+++ controllers/shop.js
@@ -117,9 +117,12 @@ exports.postDeleteCartItem = (req, res, next) => {
 };
 
 exports.postOrder = (req, res, next) => {
+  let fetchedCart;
+
   req.user
     .getCart()
     .then(cart => {
+      fetchedCart = cart;
       return cart.getProducts();
     })
     .then(products => {
@@ -134,6 +137,9 @@ exports.postOrder = (req, res, next) => {
               return product;
             })
           );
+        })
+        .then(result => {
+          return fetchedCart.setProducts(null);
         })
         .then(result => {
           res.redirect('/orders');
