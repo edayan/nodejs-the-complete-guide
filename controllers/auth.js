4a935c7387cb811529b15d401e168dcafa6fdfa9
Saju Paul <saju.christel@blu-pa.com>
2019-10-30 18:35:27 +0530

    implementing logout

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,26 @@
 exports.getLogin = (req, res, next) => {
-  const isLoggedIn = req.get('Cookie').split('=')[1] === 'true';
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
-    isAuthenticated: isLoggedIn
+    isAuthenticated: true
   });
 };
 
 exports.postLogin = (req, res, next) => {
-  res.setHeader('Set-Cookie', 'loggedIn=true');
-  res.redirect('/');
+  User.findByPk(1)
+    .then(user => {
+      req.session.isLoggedIn = true;
+      req.session.user = user;
+      res.redirect('/');
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
+
+exports.postLogOut = (req, res, next) => {
+  req.session.destroy(err => {
+    console.log(err);
+    res.redirect('/');
+  });
 };
