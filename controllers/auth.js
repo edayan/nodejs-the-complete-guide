9615d213124db368a764183be2e46919ac9b4995
Saju Paul <saju.christel@blu-pa.com>
2019-11-07 21:07:05 +0530

    implementing password reset

--- controllers/auth.js
+++ controllers/auth.js
@@ -196,8 +196,33 @@ exports.getNewPassword = (req, res, next) => {
         path: '/new-password',
         pageTitle: 'New Password',
         errorMessage: message,
-        userId: user.id
+        userId: user.id,
+        passwordToken: token
       });
     })
     .catch(err => console.log(err));
+};
+
+exports.postNewPassword = (req, res, next) => {
+  const newPassword = req.body.password;
+  const userId = req.body.userId;
+  const passwordToken = req.body.passwordToken;
+  let resetUser;
+  User.findOne({ where: { email: `saju.edayan@gmail.com` } }) //actually extract from token by going to db.
+    .then(user => {
+      resetUser = user;
+      return bcrypt.hash(newPassword, 12);
+    })
+    .then(hashedPassword => {
+      resetUser.password = hashedPassword;
+      resetUser.resetToken = null;
+      resetUser.resetTokenExpiration = null;
+      return resetUser.save();
+    })
+    .then(result => {
+      res.redirect('/login');
+    })
+    .catch(err => {
+      console.log(err);
+    });
 };
