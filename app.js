29984a4ad52bd3c173eedd69d07398599a83e303
Saju Paul <saju.christel@blu-pa.com>
2019-12-04 20:41:33 +0530

    putting csrf infront so that every request will have that, and redering error page

--- app.js
+++ app.js
@@ -46,6 +46,12 @@ app.use(csrfProtection);
 app.use(flash());
 
 app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
+});
+
+app.use((req, res, next) => {
   if (!req.session.user) {
     return next();
   }
@@ -62,12 +68,6 @@ app.use((req, res, next) => {
     });
 });
 
-app.use((req, res, next) => {
-  res.locals.isAuthenticated = req.session.isLoggedIn;
-  res.locals.csrfToken = req.csrfToken();
-  next();
-});
-
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
@@ -76,7 +76,7 @@ app.get('/500', errorController.get500);
 app.use(errorController.get404);
 
 app.use((error, req, res, next) => {
-  res.redirect('/500');
+  res.status(500).render('500', { pageTitle: 'Error occured', path: '/500' });
 });
 
 Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
